6ca49e009c824be80cd99a06386dd87db302bee5
Conrad-Push <kpusz@student.agh.edu.pl>
2021-06-10 16:59:16 +0200

    some changes

--- D10/file03.php
+++ D10/file03.php
@@ -9,8 +9,9 @@ printf("Query failed: %s\n", mysqli_error($link)); //obsługa błędu pobierania
 }
 printf("<h1>BILETY</h1>");
 printf("<table border=\"1\">"); printf("<tr><th>NAZWA</th><th>CENA</th><th>ILOSC</th></tr>");
-($row = mysqli_fetch_assoc($result)) //funkcja pobierająca dane z tabeli w formie słownika
-printf("<tr><td>%s</td><td>%6.2f</td><td>%6.2f</td></tr>", $row["NAZWA"],$row["CENA"],$row["ILOSC"]); printf("</table>"); //wyswietlenie poszczególnych kolumn
+($row = mysqli_fetch_assoc($result)); //funkcja pobierająca dane z tabeli w formie słownika
+printf("<tr><td>%s</td><td>%6.2f</td><td>%6.2f</td></tr>",
+$row["NAZWA"],$row["CENA"],$row["ILOSC"]); printf("</table>"); //wyswietlenie poszczególnych kolumn
 printf("<i>query returned %d rows </i>", mysqli_num_rows($result)); //funkcja zwracająca ilość pobranych wierszy
 mysqli_free_result($result); //wyczyszczenie pamięci
 mysqli_close($link); //zamknięcie połączenia
