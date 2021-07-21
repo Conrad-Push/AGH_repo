e627f9dd5a7717452e6ac1357deb7664c0e05811
Conrad-Push <kpusz@student.agh.edu.pl>
2021-07-22 01:20:04 +0200

    Block background when navi-bar-hamburger acvtive

--- Project_Ziolkowski/scripts.js
+++ Project_Ziolkowski/scripts.js
@@ -2,11 +2,26 @@ const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li');
+    const gallery = document.querySelector('.gallery');
+    const cardm = document.querySelector('.card-m');
+    const cardmp = document.querySelector('.card-mp');
+    const cardp = document.querySelector('.card-p');
     
     burger.addEventListener('click', ()=> {
         //Toggle Nav
         nav.classList.toggle('nav-active');
+        if(gallery)
+        {
+            gallery.classList.toggle('gallery-block');
 
+        }
+        else if(cardm && cardmp && cardp)
+        {
+            cardm.classList.toggle('cards-block');
+            cardmp.classList.toggle('cards-block');
+            cardp.classList.toggle('cards-block');
+        }
+        
         //Animate Links
         navLinks.forEach((link, index)=>{
             if (link.style.animation) {
