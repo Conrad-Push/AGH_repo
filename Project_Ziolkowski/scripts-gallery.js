85bfa82700c8985eb980f308dacfa096ee702698
Conrad-Push <kpusz@student.agh.edu.pl>
2021-07-23 15:35:13 +0200

    Add modal with interactive google map

--- Project_Ziolkowski/scripts-gallery.js
+++ Project_Ziolkowski/scripts-gallery.js
@@ -1,13 +1,12 @@
 const modal = document.querySelector(".modal");
 const previews = document.querySelectorAll(".gallery img");
 const original = document.querySelector(".full-img");
-const imgText = document.querySelector(".caption");
 
 previews.forEach((preview) => {
     preview.addEventListener("click", () => {
         modal.classList.add("open");
         original.classList.add("open");
-        //Dynamic change text and image
+        //Dynamic change image
         const originalSrc = preview.getAttribute("data-original");
         original.src = `./Strona_img/${originalSrc}`;
     });
